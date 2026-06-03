7077ad47d27b870cf6254e88c60417623af34caf
Vineeth Pawar <vineethpawar99@gmail.com>
2026-06-04 00:42:00 +0530

    chore(primitives): barrel + package.json exports for the new comps

--- packages/primitives/src/index.ts
+++ packages/primitives/src/index.ts
@@ -2,3 +2,18 @@ export { Box } from "./box";
 export type { BoxProps } from "./box";
 export { boxConfig } from "./box/config";
 export type { BoxVariants } from "./box/config";
+
+export { Text } from "./text";
+export type { TextProps, TextSize, TextWeight, TextAlign } from "./text";
+
+export { Stack } from "./stack";
+export type { StackProps, StackDirection, StackAlign, StackJustify } from "./stack";
+
+export { Flex } from "./flex";
+export type { FlexProps } from "./flex";
+
+export { Input } from "./input";
+export type { InputProps, InputSize } from "./input";
+
+export { Button } from "./button";
+export type { ButtonProps, ButtonVariant, ButtonSize } from "./button";
